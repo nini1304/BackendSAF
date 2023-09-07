479c7c9fbbeec461ed57f271c13af019f24942f9
calc <geraldine.ibieta13@gmail.com>
2023-09-07 01:43:54 -0400

    Creacion del proyecto

---
+++ src/main/java/com/example/BackendSAF/BackendSafApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackendSAF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BackendSAF/BackendSafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackendSAF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
