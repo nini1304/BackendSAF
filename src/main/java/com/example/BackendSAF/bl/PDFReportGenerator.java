144d24b2826b41f5a8e3634f94aaca50e6468139
charlie2913 <carlos.camargo.l@ucb.edu.bo>
2023-11-19 19:02:43 -0400

    Añadida paginacion al reporte PDF

--- src/main/java/com/example/BackendSAF/bl/PDFReportGenerator.java
+++ src/main/java/com/example/BackendSAF/bl/PDFReportGenerator.java
@@ -2,6 +2,7 @@ package com.example.BackendSAF.bl;
 
 import java.awt.*;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
@@ -9,9 +10,7 @@ import java.util.List;
 import com.example.BackendSAF.dto.ActivoFijoList2Dto;
 import com.lowagie.text.*;
 import com.lowagie.text.Font;
-import com.lowagie.text.pdf.PdfPCell;
-import com.lowagie.text.pdf.PdfPTable;
-import com.lowagie.text.pdf.PdfWriter;
+import com.lowagie.text.pdf.*;
 
 public class PDFReportGenerator {
     public static void generatePDFReport(List<ActivoFijoList2Dto> data, String fileName) {
@@ -77,34 +76,36 @@ public class PDFReportGenerator {
     }
     public static void generatePDFReport2(List<ActivoFijoList2Dto> data, String fileName) {
         Document document = new Document();
+        PdfWriter writer = null;
         try {
-            PdfWriter.getInstance(document, new FileOutputStream(fileName));
+            writer = PdfWriter.getInstance(document, new FileOutputStream(fileName));
+            writer.setPageEvent(new PageNumberEventHandler());
             document.open();
 
             Font titleFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 18);
-            Font headerFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 12);
-            Font contentFont = FontFactory.getFont(FontFactory.HELVETICA, 12);
+            Font headerFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 8);
+            Font contentFont = FontFactory.getFont(FontFactory.HELVETICA, 8);
 
             Paragraph title = new Paragraph("Informe de Activos Fijos", titleFont);
             title.setAlignment(Element.ALIGN_CENTER);
             document.add(title);
 
-            PdfPTable table = new PdfPTable(9); // Reducir el número de columnas a 9
+            PdfPTable table = new PdfPTable(8); // Reducir el número de columnas a 9
             table.setWidthPercentage(100);
             table.setSpacingBefore(10f);
             table.setSpacingAfter(10f);
 
             // Encabezados de columna
             String[] headers = {
-                    "ID", "Nombre", "Valor", "Fecha de Compra", "Descripción", "Tipo de Activo", "Marca",
-                    "% Depreciación", "Depreciación"
+                    "ID", "Nombre", "Valor", "Fecha de Compra", "Tipo de Activo", "Marca",
+                    "Porcentaje Depreciación", "Depreciación"
             };
 
             for (String header : headers) {
                 PdfPCell headerCell = new PdfPCell();
                 headerCell.addElement(new Phrase(header, headerFont));
                 headerCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-                headerCell.setBackgroundColor(Color.CYAN); // Fondo gris para encabezados
+                headerCell.setBackgroundColor(Color.pink); // Fondo gris para encabezados
                 headerCell.setBorderWidth(1); // Agregar bordes
                 table.addCell(headerCell);
             }
@@ -115,7 +116,7 @@ public class PDFReportGenerator {
                 table.addCell(createCell(activo.getNombre(), contentFont));
                 table.addCell(createCell(activo.getValor().toString(), contentFont));
                 table.addCell(createCell(activo.getFechaCompra(), contentFont));
-                table.addCell(createCell(activo.getDescripcion(), contentFont));
+                //table.addCell(createCell(activo.getDescripcion(), contentFont));
                 table.addCell(createCell(activo.getTipoActivoNombre(), contentFont));
                 table.addCell(createCell(activo.getMarcaNombre(), contentFont));
                 table.addCell(createCell(activo.getPorcentajeDepreciacion().toString(), contentFont));
@@ -128,6 +129,10 @@ public class PDFReportGenerator {
             System.out.println("PDF report generated: " + fileName);
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
         }
     }
 
@@ -143,5 +148,19 @@ public class PDFReportGenerator {
                 new ActivoFijoList2Dto(2L, "Activo 2", new BigDecimal(200), LocalDate.now().toString(), "Descripción 2", "Tipo 2", "Marca 2", "Calle 2", "Avenida 2", "Bloque 2", "Ciudad 2", "Personal 2", "Estado 2", "Condición 2",0, new BigDecimal(20), new BigDecimal(20)),
                 new ActivoFijoList2Dto(3L, "Activo 3", new BigDecimal(300), LocalDate.now().toString(), "Descripción 3", "Tipo 3", "Marca 3", "Calle 3", "Avenida 3", "Bloque 3", "Ciudad 3", "Personal 3", "Estado 3", "Condición 3",0, new BigDecimal(30), new BigDecimal(30)));
                 //generatePDFReport(data, "C:\\Users\\ccama\\OneDrive\\Escritorio\\reporte.pdf");
+    }
+    // Clase para manejar eventos de numeración de páginas
+    static class PageNumberEventHandler extends PdfPageEventHelper {
+        public void onEndPage(PdfWriter writer, Document document) {
+            PdfContentByte cb = writer.getDirectContent();
+            cb.beginText();
+            try {
+                cb.setFontAndSize(BaseFont.createFont(), 8); // Establecer fuente y tamaño
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            cb.showTextAligned(PdfContentByte.ALIGN_RIGHT, "Page " + writer.getPageNumber(), document.right() - 10, document.bottom() - 10, 0);
+            cb.endText();
+        }
     }
 }
